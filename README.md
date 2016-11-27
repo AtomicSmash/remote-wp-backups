61c2c93ed83708f8f4868fd2ee91f2d3063d781c
David Darke <david@atomicsmash.co.uk>
2016-11-27 18:02:37 +0000

    Added current script

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# remote-wp-backups
+# Remote Wordpress backup via WP-CLI
+
+This script is for backing up a live Wordpress website from a remote server. It doesn't require any wordpress plugins to be installed, but it does require:
+
+1. Two distinct server environments on separate servers.
+2. SSH access to both servers
+3. Ability to be able to create a 'wp-backup' user on both servers
+
+sudo adduser wp-backup
+
+### How it works
+
+It's pretty simple, the script (on your backup server) will SSH into your live server zip 
+
+[image of server layout]
+
+setup crontab with:
+
+```
+0 * * * * ~/dropbox-database-backup/backup.sh &> /dev/null
+```
+
+
+
+
+### Setup steps on live server
+
+1. adduser wp-backup
+1. mkdir -p ~/backups/files
+1. mkdir -p ~/backups/sql
+
+
+
+## Disclaimer
+Even though we use this script on an daily across multiple servers we do enforce that you use this script at your own risk and we **are not responsible** for any detrimental effects that might occur from using. If you are unsure about what the script actually does or how it works, please do not use it without reviewing throughly.
+
+
+## Notes
+
+This script is Wordpress related but could easily
---
+++ backupWordpressSite.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function makeSQLBackup() {
+    echo "------- BACKING UP $1 LIVE DB -------";
+    # Create backup folder and export db
+    ssh $2@$3 -p $4 "mkdir -p $6/sql && wp db export - --path=$5 --allow-root | gzip > $6/sql/$1-$CURRENT_TIMESTAMP.sql.gz";
+    echo "------- COMPLETED BACKING UP $1 LIVE DB -------";
+    # echo "------- BACKING UP $1 TO BACKUP SERVER-------";
+}
+
+function makeFILEBackup() {
+    echo "------- BACKING SITES FOLDER FOR $1 -------";
+    # Create backup folder and export db
+    ssh $2@$3 -p $4 "mkdir -p $6/files && zip -r -q $6/files/$1-$CURRENT_DATE.zip $5";
+    echo "------- COMPLETE BACKING UPLOADS FOLDER FOR $1 -------";
+}
+
+function grabBackups() {
+    echo "------- SYNCING BACKUPS FOR $1 -------";
+    # Get files onto backup server
+    rsync -e "ssh -p $4" -r --size-only $2@$3:$6/* $6/$1
+    echo "------- COMPLETE SYNCING BACKUPS FOR $1 -------";
+}
+
+function cleanupBackups() {
+
+    # find ~/Music/* -mtime +5 -exec ls \;
+
+
+    # ssh $2@$3 -p $4 "find $6/* -mtime +20";
+
+    # Remove backups older than 2 days form the live server
+    # ssh $2@$3 -p $4 "find $6/* -mtime +2 -exec rm -rf {} \;";
+
+    # Remove backups from backup server
+    # find $6/* -mtime +2 -exec rm -rf {} \;
+    find $6/* -mtime +2
+
+
+
+}
+
+# Set variables
+CURRENT_TIMESTAMP="`date +%d-%m-%Y_%H-%M`"
+CURRENT_DATE="`date +%d-%m-%Y`"
+
+
+# makeSQLBackup $1 $2 $3 $4 $5 $6
+
+# makeFILEBackup $1 $2 $3 $4 $5 $6
+
+# grabBackups $1 $2 $3 $4 $5 $6
+
+cleanupBackups $1 $2 $3 $4 $5 $6
